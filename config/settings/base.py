b49d54657b0d195144d8cf2dce9addc906f8c7dc
dandresfsoto@gmail.com <maquinas2008%>
2020-07-13 12:13:55 -0500

    Update media folder, v2

--- config/settings/base.py
+++ config/settings/base.py
@@ -206,7 +206,7 @@ STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
 
-MEDIA_ROOT = str(APPS_DIR / "media")
+MEDIA_ROOT = str(ROOT_DIR / "media")
 
 MEDIA_URL = "/media/"
 
